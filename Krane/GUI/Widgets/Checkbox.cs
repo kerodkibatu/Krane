40777ac9f1b0370d036c3ba25b516b36c40e7d01
Ker-Verse <kerod5858@gmail.com>
2022-08-31 02:34:11 +0300

    made fully functional

--- Krane/GUI/Widgets/Checkbox.cs
+++ Krane/GUI/Widgets/Checkbox.cs
@@ -1,30 +1,50 @@
-﻿using Krane.Interactive;
-using System.Drawing;
+﻿using Krane.Core;
+using Krane.Interactive;
 
 namespace Krane.GUI.Widgets;
 public class Checkbox : Widget
 {
     public static uint DefaultSize = 32;
-    public override bool Visible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override bool Visible { get; set; } = true;
     bool wasClicked = false;
     public override Vector2f Position => throw new NotImplementedException();
-    public EventHandler? StateChanged { get; set; }
+
+
+    public EventHandler<bool>? StateChanged { get; set; }
     public RectangleShape ToggleArea;
-    Label label;
-    public Checkbox()
+    public RectangleShape Indicator;
+    bool _checked = false;
+    public bool Checked { get { return _checked; } set { StateChanged?.Invoke(this, _checked = value); } }
+    Label Label;
+    public Checkbox(string text, Vector2f Position, uint? Size = null, Color? FillColor = null, Color? Outline = null, Color? TextColor = null)
     {
-
-        ToggleArea = new();
+        ToggleArea = new(new Vector2f(Size??DefaultSize, Size??DefaultSize))
+        {
+            Position = Position,
+            FillColor = Color.Transparent,
+            OutlineColor = Outline??Color.White,
+            OutlineThickness = 1
+        };
+        Indicator = new(ToggleArea.Size * 0.9f)
+        {
+            Position = ToggleArea.Position+ToggleArea.Size/2f,
+            FillColor = FillColor??Color.White
+        };
+        Indicator.Origin = Indicator.Size / 2f;
+        Label = new(text, new(Position.X + ToggleArea.Size.X * 1.05f, Position.Y),FillColor:TextColor,TextSize:Size??DefaultSize);
     }
-
+    public bool Toggle()
+    {
+        return Checked = !Checked;
+    }
     public override void Update()
     {
         if (!Visible)
             return;
         var MousePos = Input.GetMousePosition();
-        if (Rectangle.GetGlobalBounds().Contains(MousePos.X, MousePos.Y) && Input.isMousePressed(Mouse.Button.Left) && !wasClicked)
+        if (ToggleArea.GetGlobalBounds().Contains(MousePos.X, MousePos.Y) && Input.isMousePressed(Mouse.Button.Left) && !wasClicked)
         {
-            StateChanged?.Invoke(this, new());
+            Toggle();
             wasClicked = true;
         }
         else if (!Input.isMousePressed(Mouse.Button.Left))
@@ -34,7 +54,12 @@ public class Checkbox : Widget
     }
     public override void Draw()
     {
-
+        if (!Visible)
+            return;
+        Render.Draw(ToggleArea);
+        if (Checked)
+            Render.Draw(Indicator);
+        Label.Draw();
     }
 
 }
