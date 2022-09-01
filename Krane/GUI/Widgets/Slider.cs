77855133c7770fa998358b1c5234dba605f7f11c
Ker-Verse <kerod5858@gmail.com>
2022-09-01 03:35:34 +0300

    Made fully functional

--- Krane/GUI/Widgets/Slider.cs
+++ Krane/GUI/Widgets/Slider.cs
@@ -1,4 +1,5 @@
 ﻿using Krane.Core;
+using Krane.Interactive;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Krane.GUI.Widgets
 {
     public class Slider : Widget
     {
-        public static Vector2f DefaultSize = new(150f,40f);
+        public static Vector2f DefaultSize = new(150f, 40f);
 
         public override bool Visible { get; set; }
 
@@ -20,10 +21,13 @@ namespace Krane.GUI.Widgets
 
         RectangleShape OuterRect;
         RectangleShape InnerRect;
-
-        
-        public Slider(string text, Vector2f Position, Vector2f? Size = null,int Inset = 5, Color? Fill = null, Color? Outline = null, Color? TextColor = null)
+        public EventHandler<float>? ValueChanged;
+        public float Value { get; set; }
+        float MinVal, MaxVal;
+        Vector2f sliderSize;
+        public Slider(float Min,float Max, Vector2f Position, Vector2f? Size = null,float Value = 0,int Inset = 5, Color? Fill = null, Color? Outline = null)
         {
+            (MinVal, MaxVal) = (Min, Max);
             OuterRect = new(Size ?? DefaultSize)
             {
                 Position = Position,
@@ -37,13 +41,35 @@ namespace Krane.GUI.Widgets
                 FillColor = Fill??Color.White,
                 OutlineColor = Color.Transparent
             };
-
+            sliderSize = InnerRect.Size;
+            this.Value = Value;
+            Refresh();
         }
-
+        public void SetVal(float newVal)
+        {
+            Value = newVal;
+            Value = Value > MaxVal ? MaxVal : Value < MinVal ? MinVal : Value;
+            ValueChanged?.Invoke(this, Value);
+            Refresh();
+        }
+        public void Refresh()
+        {
+            InnerRect.Size = new Vector2f(sliderSize.X*map(Value,MinVal,MaxVal,0,1),sliderSize.Y);
+        }
+        float map(float s, float a1, float a2, float b1, float b2)
+        {
+            return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+        }
         public override void Update()
         {
             if (!Visible)
                 return;
+            var MousePos = Input.GetMousePosition();
+            if (OuterRect.GetGlobalBounds().Contains(MousePos.X, MousePos.Y) && Input.IsButtonPressed(Mouse.Button.Left))
+            {
+                var Bounds = OuterRect.GetGlobalBounds();
+                SetVal(map(MousePos.X,Bounds.Left,Bounds.Width,MinVal,MaxVal));
+            }
         }
         public override void Draw()
         {
