670701bb7366bdc1d7b15f5e73b09889578bb0d6
KerVerse <kerod5858@gmail.com>
2024-06-18 07:19:15 +0300

    Added GUI Support and Experimental Widget API

---
+++ Krane/GUI/GUIManager.cs
@@ -0,0 +1,56 @@
+﻿namespace Krane.GUI;
+public static class GUIManager
+{
+    static Dictionary<string, WidgetGroup> Groups;
+    public static WidgetGroup? ActiveGroup;
+    static GUIManager()
+    {
+        Groups = new();
+    }
+    public static void AddGroup(string name,WidgetGroup group)
+    {
+        Groups.Add(name,group);
+        SetActiveGroup(name);
+    }
+    public static WidgetGroup GetGroup(string name)
+    {
+        return Groups[name]??throw new NullReferenceException();
+    }
+    public static void Hide()
+    {
+        foreach (var group in Groups)
+        {
+            group.Value.Hide();
+        }
+    }
+    public static void Hide(string name)
+    {
+        GetGroup(name).Hide();
+    }
+    public static void Show()
+    {
+        foreach (var group in Groups)
+        {
+            group.Value.Show();
+        }
+    }
+    public static void Show(string name)
+    {
+        GetGroup(name).Show();
+    }
+    public static void SetActiveGroup(string name)
+    {
+        Hide();
+        Show(name);
+        ActiveGroup = GetGroup(name);
+    }
+    public static void Update()
+    {
+        ActiveGroup?.Update();
+    }
+    public static void Draw()
+    {
+        ActiveGroup?.Draw();
+    }
+
+}
---
+++ Krane/GUI/Widgets/Button.cs
@@ -0,0 +1,62 @@
+﻿using Krane.Core;
+using Krane.Resources;
+using Krane.Interactive;
+
+namespace Krane.GUI.Widgets;
+public class Button : Widget
+{
+    public static Vector2f DefaultSize = new(150, 50);
+
+    RectangleShape Rectangle;
+    bool wasClicked = false;
+
+    public Label Label;
+    public override bool Visible { get; set; } = true;
+    public override Vector2f Position => Rectangle.Position;
+    public string DisplayedText => Label.DisplayedText;
+    public EventHandler? Clicked { get; set; }
+    public Color BackColor => Rectangle.FillColor;
+    public Color ForeColor => Label.FillColor;
+    public Button(string text, Vector2f Position, Vector2f? Size = null, Color? FillColor = null, Color? Outline = null,Color? TextColor = null,uint TextSize = 32)
+    {
+        Rectangle = new(Size ?? DefaultSize)
+        {
+            Position = Position,
+            FillColor = FillColor ?? Color.White,
+            OutlineColor = Outline ?? Color.Transparent,
+            OutlineThickness = 1
+        };
+        Label = new(text, Rectangle.Position+Rectangle.Size/2f,true,FillColor:TextColor,TextSize: TextSize);
+    }
+    public void SetColor(Color Fill, Color? Outline = null)
+    {
+        Rectangle.FillColor = Fill;
+        Rectangle.OutlineColor = Outline ?? Color.Transparent;
+    }
+    public void SetTextColor(Color Fill, Color? Outline = null)
+    {
+        Label.SetTextColor(Fill, Outline);
+    }
+    public override void Update()
+    {
+        if (!Visible)
+            return;
+        var MousePos = Input.MousePos;
+        if (Rectangle.GetGlobalBounds().Contains(MousePos.X, MousePos.Y) && Input.IsMouseDown(Mouse.Button.Left) && !wasClicked)
+        {
+            Clicked?.Invoke(this, new());
+            wasClicked = true;
+        }
+        else if (!Input.IsMouseDown(Mouse.Button.Left))
+        {
+            wasClicked = false;
+        }
+    }
+    public override void Draw()
+    {
+        if (!Visible)
+            return;
+        Render.Target?.Draw(Rectangle);
+        Label.Draw();
+    }
+}
